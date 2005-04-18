562e610966781a2050f5f7a6461b342147516738
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2005-04-18 05:44:16 +0000

    lots of work tonight!
    git-svn-id: svn://okws.org/trunk/sfslite2@759 7d287422-6eea-da11-b677-00123f2a783a

---
+++ Attic/python/ex1/ex1.py
@@ -0,0 +1,27 @@
+
+import ex1
+import async
+import socket
+
+def cb(e,r):
+    print "e=", e, "& r=", r
+
+
+sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+sock.connect (('127.0.0.1', 3000))
+fd = sock.fileno ()
+
+print "file descriptor is", fd
+
+x = async.arpc.axprt_stream (fd)
+cli = async.arpc.aclnt (x, ex1.foo_prog_1 ())
+
+f = ex1.foo_t ()
+f.x = 'this is a test string'
+f.xx = 1010
+
+
+cli.call (ex1.FOO_FUNC, f, cb);
+
+async.core.amain ()
+
