dd69fc973f814c7ac147ba7d1c156d65860bffcf
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2005-04-20 18:28:43 +0000

    tested for memory decrefs
    git-svn-id: svn://okws.org/trunk/sfslite2@770 7d287422-6eea-da11-b677-00123f2a783a

--- Attic/python/ex1/ex1.py
+++ Attic/python/ex1/ex1.py
@@ -1,10 +1,28 @@
 
+#
+# simple test program
+#
+# $Id$
+#
+
 import ex1
 import async
 import socket
 
 def cb(err,res):
     print "err=", err, "& res=", res
+
+#
+# make call a function, so that way we can test the refcounting on 
+# args to arpc.call calls.  if we called from main, then we would
+# never see f being dealloced (since main will always have a reference
+# to it)
+#
+def call(cli):
+    f = ex1.foo_t ()
+    f.x = 'this is a test string'
+    f.xx = 1010
+    cli.call (ex1.FOO_FUNC, f, cb)
 
 
 sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
@@ -16,12 +34,8 @@ print "file descriptor is", fd
 x = async.arpc.axprt_stream (fd)
 cli = async.arpc.aclnt (x, ex1.foo_prog_1 ())
 
-f = ex1.foo_t ()
-f.x = 'this is a test string'
-f.xx = 1010
+call (cli);
 
-
-cli.call (ex1.FOO_FUNC, f, cb)
 
 async.core.amain ()
 
