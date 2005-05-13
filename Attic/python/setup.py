3630615f1115c99e36e7ca366595e38bbb105544
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2005-05-13 12:50:01 +0000

    hacked more on this setup thing.  still todo:  - find a way to toggle -O3 on/off  - find a way to turn pthreads on/off  - find a way to use g++  - find a way to give -Xlinker -rpath foo args to the link line
    git-svn-id: svn://okws.org/trunk/sfslite2@850 7d287422-6eea-da11-b677-00123f2a783a

--- Attic/python/setup.py
+++ Attic/python/setup.py
@@ -1,43 +1,93 @@
+#!/usr/bin/env python
+
+"""\
+=========================
+Python interface to SFS
+=========================
+
+Python interface to the libasync/SFS libraries of David
+Mazierers (www.fs.net)
+
+Design goals are:
+
+- use libasync event loop
+
+- python interfaces to RPC libraries and compilers.
+
+pysfs is `Free Software`_.
+
+.. _MySQL: http://www.mysql.com/
+.. _`Free Software`: http://www.gnu.org/
+.. [PEP-0249] http://www.python.org/peps/pep-0249.html
+
+"""
+
+import os
+import sys
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+name = "SFS-%s" % os.path.basename(sys.executable)
+version = "0.1"
+extra_objects = []
+extra_compile_args = []
+
+libraries = [ 'async', 'arpc', 'sfscrypt', 'pyarpc', 'gmp' ]
+library_dirs = [ '/usr/local/lib/sfslite/shdbg' ]
+include_dirs = [ '/usr/local/include/sfslite/shdbg' ]
+
+include_dirs = [ d for d in include_dirs if os.path.isdir (d) ]
+library_dirs = [ d for d in library_dirs if os.path.isdir (d) ]
+
+classifiers = """
+Development Status :: 5 - Production/Stable
+Environment :: Other Environment
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: MacOS :: MacOS X
+Operating System :: OS Independent
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Operating System :: Unix
+Programming Language :: C++
+Programming Language :: Python
+Topic :: Libraries
+"""
+
 
 metadata = {
-    'name': 'pyasync',
-    'version': '0.1',
-    'description': "Python wrappers for libasync",
+    'name': name,
+    'version': version,
+    'description': "Python wrappers for SFS/libasync",
+    'long_description': __doc__,
     'author': "Max Krohn",
     'author_email': "max@maxk.org",
     'license': "GPL",
     'platforms': "ALL",
-    'url': "http://sourceforge.net/projects/mysql-python",
-    'download_url': "http://prdownloads.sourceforge.net/mysql-python/" \
-                    "MySQL-python-%s.tar.gz" % version,
+    'url': "http://www.maxk.org",
+    'download_url': "http://foo.foo.com",
     'classifiers': [ c for c in classifiers.split('\n') if c ],
     'py_modules': [
-        "_mysql_exceptions",
-        "MySQLdb.converters",
-        "MySQLdb.connections",
-        "MySQLdb.cursors",
-        "MySQLdb.sets",
-        "MySQLdb.times",
-        "MySQLdb.stringtimes",
-        "MySQLdb.mxdatetimes",
-        "MySQLdb.pytimes",
-        "MySQLdb.constants.CR",
-        "MySQLdb.constants.FIELD_TYPE",
-        "MySQLdb.constants.ER",
-        "MySQLdb.constants.FLAG",
-        "MySQLdb.constants.REFRESH",
-        "MySQLdb.constants.CLIENT",
+        "async.util",
+        "async.__init__"
         ],
     'ext_modules': [
-        Extension(
-            name='async/core',
-            sources=['async.core'],
-            include_dirs=['']
-            library_dirs=library_dirs,
-            libraries=libraries,
-            extra_compile_args=extra_compile_args,
-            extra_objects=extra_objects,
-            ),
+        Extension( name='async.core',
+                   sources=['async/core.C'],
+                   include_dirs=include_dirs,
+                   library_dirs=library_dirs,
+                   libraries=libraries,
+                   extra_compile_args=extra_compile_args,
+                   extra_objects=extra_objects,
+                   ),
+        Extension( name='async.rpctypes',
+                   sources=['async/rpctypes.C'],
+                   include_dirs=include_dirs,
+                   library_dirs=library_dirs,
+                   libraries=libraries,
+                   extra_compile_args=extra_compile_args,
+                   extra_objects=extra_objects,
+                   )
         ],
     }
 
