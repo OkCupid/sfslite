1489a3db42aaf279d9dcfb2dd9f41d1d352b6e75
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2005-11-09 21:04:42 +0000

    cool
    git-svn-id: svn://okws.org/trunk/sfslite2@1130 7d287422-6eea-da11-b677-00123f2a783a

--- unwrap/unwrap.h
+++ unwrap/unwrap.h
@@ -2,12 +2,22 @@
 #ifndef _UNWRAP_H
 #define _UNWRAP_H
 
-struct yystype {
+#include "amisc.h"
+#include "vec.h"
+#include "union.h"
+#include "qhash.h"
+
+struct YYSTYPE {
   ::str str;
   char ch;
 };
 
-#define YYSTYPE yystype
 extern YYSTYPE yylval;
+
+extern int yylex ();
+extern int yyparse ();
+#undef yyerror
+extern int yyerror (str);
+
 
 #endif /* _UNWRAP_H */
