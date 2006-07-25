db5bbe0555f9f0a05de095cb3430c63cd9d2bffa
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-07-25 01:28:55 +0000

    Compile fix in OKWS
    git-svn-id: svn://okws.org/sfslite2/trunk@2124 7d287422-6eea-da11-b677-00123f2a783a

--- async/callback.h
+++ async/callback.h
@@ -2,12 +2,17 @@
 // -*-c++-*-
 /* $Id: async.h 1759 2006-05-19 23:54:14Z max $ */
 
-#include "config.h"
+// NEED SFS AUTOCONF.H!
+#include "autoconf.h"
 
-#ifdef SFS_HAVE_CALLBACK2
-# include "callback2.h"
-# define SIGNAL(cb, ...) (cb)->signal (__VA_ARGS__)
-#else
-# include "callback1.h"
-# define SIGNAL(cb, ...) (*cb) (__VA_ARGS__)
-#endif /* SFS_HAVE_CALLBACK2 */
+#ifndef _CALLBACK_H_INCLUDED_
+
+# ifdef SFS_HAVE_CALLBACK2
+#  include "callback2.h"
+#  define SIGNAL(cb, ...) (cb)->signal (__VA_ARGS__)
+# else
+#  include "callback1.h"
+#  define SIGNAL(cb, ...) (*cb) (__VA_ARGS__)
+# endif /* SFS_HAVE_CALLBACK2 */
+
+#endif /* _CALLBACK_H_INCLUDED_ */
