6d66db438a03f26da8c426ba9685fe302d16fe47
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-09-27 19:45:21 +0000

    tmp
    git-svn-id: svn://okws.org/sfslite2/devel/pth-0-1@2212 7d287422-6eea-da11-b677-00123f2a783a

---
+++ libtame/tame.h
@@ -0,0 +1,12 @@
+
+// -*-c++-*-
+/* $Id: tame.h 2206 2006-09-27 18:24:21Z max $ */
+
+#ifndef _ASYNC_TAME_H_
+#define _ASYNC_TAME_H_
+
+#include "tame_core.h"
+#include "tame_cancel.h"
+#include "tame_mkevent.h"
+
+#endif /* _ASYNC_TAME_H_ */
