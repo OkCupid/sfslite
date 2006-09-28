4d47dfc57f86052ba414ef138bfc8f25bcbb5e98
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-09-28 16:47:38 +0000

    weird how this slipped by
    git-svn-id: svn://okws.org/sfslite2/devel/concur-0-1@2227 7d287422-6eea-da11-b677-00123f2a783a

--- arpc/axprt.h
+++ arpc/axprt.h
@@ -48,7 +48,7 @@ public:
   virtual void setrcb (recvcb_t) = 0;
   virtual bool ateof () { return false; }
   virtual u_int64_t get_raw_bytes_sent () const { return 0; }
-  virtual int sndbufsize () const { panic ("unimplemented"); }
+  virtual int sndbufsize () const { panic ("unimplemented"); return 0; }
   virtual void poll () = 0;
   virtual int getfd () = 0;
 
