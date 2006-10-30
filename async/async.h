622394711a0f3e98115f1d60b601d4f7952f6db8
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-10-30 14:21:39 +0000

    version bump
    git-svn-id: svn://okws.org/sfslite2/trunk@2298 7d287422-6eea-da11-b677-00123f2a783a

--- async/async.h
+++ async/async.h
@@ -89,7 +89,7 @@ extern u_int64_t ifchg_count;
 ifchgcb_t *ifchgcb (cbv);
 void ifchgcb_remove (ifchgcb_t *chg);
 
-#define SFSLITE_PATCHLEVEL 8009100
-#define SFSLITE_PATCHLEVEL_STR "0.8.9"
+#define SFSLITE_PATCHLEVEL 8010100
+#define SFSLITE_PATCHLEVEL_STR "0.8.10"
 
 #endif /* !_ASYNC_ASYNC_H_ */
