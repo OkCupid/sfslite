b8dd22b899f1b4d7a4129c30a3386717c36912af
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2007-02-02 02:13:58 +0000

    Release 0.8.15
    git-svn-id: svn://okws.org/sfslite2/trunk@2521 7d287422-6eea-da11-b677-00123f2a783a

--- async/async.h
+++ async/async.h
@@ -92,7 +92,7 @@ void ifchgcb_remove (ifchgcb_t *chg);
 #define SFSLITE_VERSION_MAJOR 0
 #define SFSLITE_VERSION_MINOR 8
 #define SFSLITE_VERSION_PATCHLEVEL 15
-#define SFSLITE_VERSION_PRE 1
+#define SFSLITE_VERSION_PRE 100
 
 #define VERSION_FLATTEN(Maj,Min,Pat,Pre) \
    (((Maj * 256 + Min) * 256 + Pat) * 256 + Pre)
@@ -104,6 +104,6 @@ void ifchgcb_remove (ifchgcb_t *chg);
                    SFSLITE_VERSION_PATCHLEVEL, \
                    SFSLITE_VERSION_PRE))
 
-#define SFSLITE_PATCHLEVEL_STR "0.8.15pre1"
+#define SFSLITE_PATCHLEVEL_STR "0.8.15"
 
 #endif /* !_ASYNC_ASYNC_H_ */
