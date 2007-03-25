46fa60d6439a446dc370b40326a9651bc81be9ea
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2007-03-25 13:54:32 +0000

    Decruft by getting rid of the tame canceller
    git-svn-id: svn://okws.org/sfslite2/devel/1.1@2631 7d287422-6eea-da11-b677-00123f2a783a

--- libtame/tame.h
+++ libtame/tame.h
@@ -2,16 +2,15 @@
 // -*-c++-*-
 /* $Id: tame.h 2206 2006-09-27 18:24:21Z max $ */
 
-#ifndef _ASYNC_TAME_H_
-#define _ASYNC_TAME_H_
+#ifndef _LIBTAME_TAME_H_
+#define _LIBTAME_TAME_H_
 
 #include "tame_event.h"
 #include "tame_core.h"
-#include "tame_cancel.h"
 #include "tame_event_ag.h"
 #include "tame_thread.h"
 #include "tame_tfork.h"
 #include "tame_trigger.h"
 #include "tame_typedefs.h"
 
-#endif /* _ASYNC_TAME_H_ */
+#endif /* _LIBTAME_TAME_H_ */
