bc9264a196bf38f3c353687f6189a4c898ccfba8
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2007-05-03 04:51:55 +0000

    Fix a bad bug in locking, wherein lock order was lifo, not fifo.
    git-svn-id: svn://okws.org/sfslite2/trunk@2844 7d287422-6eea-da11-b677-00123f2a783a

--- libtame/lock.h
+++ libtame/lock.h
@@ -24,16 +24,17 @@ public:
     waiter_t (mode_t m, cbv c) : _mode (m), _cb (c) {}
     mode_t _mode;
     cbv _cb;
-    list_entry<waiter_t> _lnk;
+    tailq_entry<waiter_t> _lnk;
   };
 
   waiter_t *acquire (mode_t m, cbv cb);
   void timed_acquire (mode_t m, u_int s, u_int ms, cbb cb, CLOSURE);
   void release ();
   void cancel (waiter_t *w);
+  mode_t mode () const { return _mode; }
 
 protected:
-  list<waiter_t, &waiter_t::_lnk> _waiters;
+  tailq<waiter_t, &waiter_t::_lnk> _waiters;
   mode_t _mode;
   void call (waiter_t *w);
 };
