51f617f465445e9aa0a750bc5e20022b3c0c54ff
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2008-09-10 18:56:56 +0000

    OK, this all seems to be working as well as it has to.
    git-svn-id: svn://okws.org/sfslite2/devel/1.2@3566 7d287422-6eea-da11-b677-00123f2a783a

--- tools/rtftp/util.C
+++ tools/rtftp/util.C
@@ -42,6 +42,26 @@ mywd ()
 
 //-----------------------------------------------------------------------
 
+static int 
+mymkdir (char *dir, char *end)
+{
+  assert (*end == '/');
+  assert (dir < end);
+  *end = 0;
+  int rc = dir_ok (dir);
+  if (rc < 0) {
+    warn ("cannot access dir %s from dir %s\n", dir, mywd ());
+  } else if (rc == 1) {
+    rc = 0;
+  } else if ((rc = mkdir (dir, 0777)) < 0) {
+    warn ("mkdir failed in %s on dir %s: %m\n", mywd (), dir);
+  }
+  *end = '/';
+  return rc;
+}
+
+//-----------------------------------------------------------------------
+
 //
 // write out the data 's' to the file given by _file.  Might need to make
 // some parent directories if the filename contains slashes.
@@ -51,36 +71,26 @@ mywd ()
 int write_file (const str &nm, const str &dat)
 {   
   char *s = strdup (nm.cstr ());
-  char *file = NULL;
-  char *np;
   int rc = 0;
-  for (char *p = s; rc >= 0 && p && *p; np = p) {
-    char *sl = strchr (p, '/');
-    if (sl && *sl) {
-      file = np = sl ++;
-      *np = 0;
-      int rc = dir_ok (p);
-      if (rc < 0) {
-	warn ("cannot access dir %s from dir %s\n", p, mywd ());
-      } else if (mkdir (p, 0777) != 0) {
-	warn ("mkdir failed in %s on dir %s: %m\n", mywd (), p);
-	rc = -1;
-      }
-      if (rc == 0 && chdir (p) != 0) {
-	warn ("cannot chdir to %s from dir %s: %m\n", p, mywd ());
-	rc = -1;
-      }
+
+  for (char *p = s; p && *p; ) {
+    p = strchr (p, '/');
+    if (p && *p) {
+      rc = mymkdir (s, p);
+      p++;
     }
   }
-  if (!file || *file) {
+
+  if (!s || !*s) {
     warn ("no file to write!\n");
     rc = -1;
-  } else if (rc == 0 && !str2file (file, dat, 0666, false)) {
-    warn ("cannot create file %s in dir %s: %m\n", file, mywd ());
+  } else if (rc == 0 && !str2file (s, dat, 0666, false)) {
+    warn ("cannot create file %s in dir %s: %m\n", s, mywd ());
     rc = -1;
   } else {
     rc = 0;
   }
+
   if (s) free (s);
   return rc;
 }
