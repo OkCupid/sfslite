3e6a538174d2608b0e11c8e4ae5440d466658da4
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-04-05 02:24:18 +0000

    A bit of work on the logger class.
    git-svn-id: svn://okws.org/sfslite2/devel/1.2@5228 7d287422-6eea-da11-b677-00123f2a783a

---
+++ libaapp/sfs_logger.h
@@ -0,0 +1,31 @@
+// -*-c++-*-
+#pragma once
+
+#include "async.h"
+#include "arpc.h"
+#include "tame.h"
+#include "tame_lock.h"
+#include "aapp_prot.h"
+
+namespace sfs {
+
+  class logger_t : public virtual refcount {
+  public:
+    logger_t (str n, int m = 0644, int tries = 5);
+    ~logger_t ();
+    void launch (evb_t ev, bool do_lock = true, CLOSURE);
+    void turn (evb_t ev, CLOSURE);
+    void log (str s, evb_t ev, CLOSURE);
+    void eofcb (ptr<bool> df);
+  private:
+    const str _file;
+    const int _mode;
+    tame::lock_t _lock;
+    int _tries;
+    pid_t _pid;
+    ptr<bool> _destroyed;
+    ptr<axprt_unix> _x;
+    ptr<aclnt> _cli;
+  };
+
+};
