205c153397fa25acc818394fc2268113c0b9fea6
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-04-06 14:31:01 +0000

    Version bump
    git-svn-id: svn://okws.org/sfslite2/devel/1.2@5234 7d287422-6eea-da11-b677-00123f2a783a

--- async/async.h
+++ async/async.h
@@ -103,7 +103,7 @@ void ifchgcb_remove (ifchgcb_t *chg);
 #define SFSLITE_VERSION_MAJOR 1
 #define SFSLITE_VERSION_MINOR 2
 #define SFSLITE_VERSION_PATCHLEVEL 8
-#define SFSLITE_VERSION_PRE 103
+#define SFSLITE_VERSION_PRE 104
 
 //
 // VERSION_PRE < 100 means pre1, pre2, etc. releases
@@ -121,6 +121,6 @@ void ifchgcb_remove (ifchgcb_t *chg);
                    SFSLITE_VERSION_PATCHLEVEL, \
                    SFSLITE_VERSION_PRE))
 
-#define SFSLITE_PATCHLEVEL_STR "1.2.8.3"
+#define SFSLITE_PATCHLEVEL_STR "1.2.8.4"
 
 #endif /* !_ASYNC_ASYNC_H_ */
