26b1341af10f89bcb68363130411d772ef17f38e
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-09-15 15:52:49 +0000

    two new files!
    git-svn-id: svn://okws.org/sfslite2/devel/1.2@5408 7d287422-6eea-da11-b677-00123f2a783a

---
+++ async/const.C
@@ -0,0 +1,6 @@
+
+#include "async.h"
+
+namespace sfs_const {
+  time_t axprt_suio_clear_interval = 0;
+};
---
+++ async/sfs_const.h
@@ -0,0 +1,6 @@
+// -*-c++-*-
+#pragma once
+
+namespace sfs_const {
+  extern time_t axprt_suio_clear_interval;
+};
